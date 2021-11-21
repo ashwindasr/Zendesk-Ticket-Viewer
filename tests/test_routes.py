451c7854eb6937ace948d9ce70525856db63098a
Ashwin Das <ashwindasr@gmail.com>
2021-11-20 20:45:40 -0500

    Changed posts endpoint to root

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,11 +8,11 @@ localhost = "http://127.0.0.1:5000"
 # Tests to be run when flask server is running
 class MyTestCase(unittest.TestCase):
     def test_1(self):
-        response = requests.get(localhost + "/posts")
+        response = requests.get(localhost + "/")
         self.equal = self.assertEqual(response.status_code, 200)
 
     def test_2(self):
-        response = requests.get(localhost + "/posts/show/1")
+        response = requests.get(localhost + "/ticket/1")
         self.equal = self.assertEqual(response.status_code, 200)
 
 
