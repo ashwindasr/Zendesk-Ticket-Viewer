01fcb69f104c2d20e9e97d486d9a819d37308eb7
Ashwin Das <ashwindasr@gmail.com>
2021-11-20 19:17:29 -0500

    Updated UI

--- routes.py
+++ routes.py
@@ -10,7 +10,7 @@ session = requests.Session()
 session.auth = CREDENTIALS
 SERVER = os.environ['MY_SERVER']
 ZENDESK = f'https://{SERVER}.zendesk.com'
-base_url = f'{ZENDESK}/api/v2/tickets'
+base_url = f'{ZENDESK}/api/v2'
 url = f'{ZENDESK}/api/v2/tickets.json?page[size]=25'
 
 
@@ -33,7 +33,13 @@ def posts_index():
         session.get(next_link).json()['tickets'] != [] \
         else False  # Checking to see if the 'next' link has any tickets or if its empty
 
-    return render_template("posts.html", posts=posts['tickets'], prev_flag=prev_flag, next_flag=next_flag)
+    total_posts_count = session.get(f"{base_url}/tickets/count").json()['count']['value']
+    current_page_count = len(posts['tickets'])
+    requester_names = get_requester_names(posts['tickets'])
+
+    return render_template("posts.html", posts=posts['tickets'], prev_flag=prev_flag, next_flag=next_flag,
+                           total_posts_count=total_posts_count, current_page_count=current_page_count,
+                           requester_names=requester_names)
 
 
 @app.route('/posts/next')
@@ -51,13 +57,32 @@ def posts_back():
 
 
 @app.route('/posts/show/<int:post_id>')
-def show(post_id):
-    show_url = base_url + f"/{post_id}"
+def show_post(post_id):
+    show_url = base_url + f"/tickets/{post_id}"
     post = get_response_json(show_url)
-    return render_template("show.html", post=post['ticket'], post_id=post_id)
+
+    requester_id = post['ticket']['requester_id']
+    requester_name = get_requester_data(requester_id)
+
+    return render_template("show.html", post=post['ticket'], post_id=post_id, requester_name=requester_name)
 
 
 # Utility function to get the json response
 def get_response_json(get_url):
     response = session.get(get_url)
     return response.json()
+
+
+# Utility function to get the email and phone number of the requester
+def get_requester_data(req_id):
+    requester_data = session.get(f"{base_url}/users/{req_id}").json()
+    requester_name = requester_data['user']['name']
+
+    return requester_name
+
+
+def get_requester_names(data):
+    names = {}
+    for row in data:
+        names[row['requester_id']] = get_requester_data(row['requester_id'])
+    return names
